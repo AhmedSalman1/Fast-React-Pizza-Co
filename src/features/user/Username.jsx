2b030c52c9e4f8547de7dd4a2d8be01c48739510
AhmedSalman <medosalman375@gmail.com>
2025-01-30 18:35:39 +0200

    style: use CSS Grid and Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-	return <div className="hidden text-sm font-semibold">Ahmed</div>;
+	return <div className="hidden text-sm font-semibold md:block">Ahmed</div>;
 }
 
 export default Username;
